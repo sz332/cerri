1ae3c0941406e6a23bb544ec50a7cdc7e796ed03
Zsolt Sandor <zsolt.sandor@gmail.com>
2018-08-16 06:35:52 +0200

    started using the graph lib for the algorithm

--- app/index.js
+++ app/index.js
@@ -1,7 +1,50 @@
 var Graph = require("graphlib").Graph;
+var Alg = require("graphlib").alg;
+var Fs = require('fs');
 
-var g = new Graph();
+function convertJsonToGraph(json) {
+    let data = {};
 
-g.setNode("a");
+    console.info("Started processing data");
 
-console.log(g.hasNode("a"));
+    data.nodes = json.nodes;
+    data.edges = [];
+
+    for (let item of json.edges) {
+        for (let toNode of item.toList) {
+
+            if (typeof toNode === 'string' || toNode instanceof String) {
+                data.edges.push({
+                    "from": item.from,
+                    "to": toNode
+                });
+            } else {
+                data.edges.push({
+                    "from": item.from,
+                    "to": toNode.name
+                });
+            }
+
+        }
+    }
+
+    console.info("Finished processing data");
+    return data;
+}
+
+let graphData = convertJsonToGraph(JSON.parse(Fs.readFileSync('data/cerri.json', 'utf8')));
+let graph = new Graph();
+
+for (let node of graphData.nodes){
+    graph.setNode(node.id, node.label);
+}
+
+for (let edge of graphData.edges){
+    graph.setEdge(edge.from, edge.to);
+}
+
+let cycles = Alg.findCycles(graph);
+
+console.log("Now what");
+
+//https://github.com/dagrejs/graphlib/wiki/API-Reference#alg-find-cycles
