940a2d3e53d419d7e2c93673f322b4bac17d0dcd
Zsolt Sandor <zsolt.sandor@gmail.com>
2018-08-18 22:48:13 +0200

    code cleanup

--- app/index.js
+++ app/index.js
@@ -3,24 +3,31 @@
 // broke the browser. Running as a separate application this shall be not a problem
 // hopefully
 
-var Graph = require("graphlib").Graph;
 var GraphLoader = require("./graphLoader.js");
 var GraphCycleFinder = require("./graphCycleFinder.js");
 var GraphMinimizer = require("./graphMinimizer.js");
 
+const {
+    performance
+} = require('perf_hooks');
+
+let start = performance.now();
+
 let graph = new GraphLoader("../data/cerri.json").load();
 
-let cycles = new GraphCycleFinder(graph).findCycles(6);
+let cycles = new GraphCycleFinder(graph).findCycles(8);
 let minimalCycles = new GraphMinimizer(graph).minimalCoveringCycles(cycles);
 
-for (let pathObject of minimalCycles.paths ){
-        console.log("\n Exercises of " + pathObject.length + " moves: \n");
+for (let pathObject of minimalCycles.paths) {
+    console.log("\n Exercises of " + pathObject.length + " moves: \n");
 
-        let counter = 1;
+    let counter = 1;
 
-        for (let path of pathObject.data){
-            console.info(counter++ + ". " + path.join(", "));
-        }
+    for (let path of pathObject.data) {
+        console.info(counter++ + ". " + path.join(", "));
+    }
 }
 
-console.info("End.");
+let duration = performance.now() - start;
+
+console.info("Ended in " + (duration / 1000) + " sec");
